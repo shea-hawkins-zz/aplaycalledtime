d500827eb3aa006ad7e07017a08778517f28069f
Silver Greed <silvergreed.goldtime@gmail.com>
2015-12-29 10:21:19 -0700

    Building semantic

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -16,7 +16,7 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'static'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
