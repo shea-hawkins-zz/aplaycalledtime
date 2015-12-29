79cced57902d5700d763a7f7ed498305f959cb17
Silver Greed <silvergreed.goldtime@gmail.com>
2015-12-29 08:43:17 -0700

    Finalizing changes

--- src/routes/WorkoutAppHomeRoute.js
+++
@@ -1,8 +0,0 @@
-import Relay from 'react-relay';
-
-export default class extends Relay.Route {
-  static queries = {
-    statBlock: () => Relay.QL`query { statBlock(id: $id) }`,
-  };
-  static routeName = 'WorkoutAppHomeRoute';
-}
